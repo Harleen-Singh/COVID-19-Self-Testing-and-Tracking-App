850faaa7d533867579fbe6471964e61181b55611
Pulkitmahajan01 <pulkitmahajan.01@gmail.com>
2020-03-23 12:48:59 +0530

    DOB,Spinner and since when u felt sick made functional

--- app/src/main/java/com/gamecodeschool/covid/homePage1.java
+++ app/src/main/java/com/gamecodeschool/covid/homePage1.java
@@ -1,14 +1,67 @@
 package com.gamecodeschool.covid;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.Toast;
 
-public class homePage1 extends AppCompatActivity {
+import java.text.DateFormat;
+import java.util.Calendar;
+
+public class homePage1 extends AppCompatActivity implements DatePickerDialog.OnDateSetListener, AdapterView.OnItemSelectedListener {
+    EditText etName,etAge,etAddress,etSubsidaryPhones;
+    ImageView ivCld;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page1);
+        etName=findViewById(R.id.etName);
+        etAge=findViewById(R.id.etAge);
+        ivCld=findViewById(R.id.ivCld);
+        ivCld.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DialogFragment datePicker =new DatePickerFragment();
+                datePicker.show(getSupportFragmentManager(),"date picker");
+            }
+        });
+        Spinner spinner=findViewById(R.id.spGender);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.sex, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+        Calendar c=Calendar.getInstance();
+        c.set(Calendar.YEAR,year);
+        c.set(Calendar.MONTH,month);
+        c.set(Calendar.DAY_OF_MONTH,dayOfMonth);
+        String currentDateString = DateFormat.getDateInstance().format(c.getTime());
+        etAge.setText(currentDateString);
+    }
+
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String text1=parent.getItemAtPosition(position).toString();
+       // Toast.makeText(parent.getContext(),text1,Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
