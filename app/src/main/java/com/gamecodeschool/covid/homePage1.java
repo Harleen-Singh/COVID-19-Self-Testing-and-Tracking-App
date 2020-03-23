abaaf8fa9c1dadf299465930e8c2e8ce1538d559
Pulkitmahajan01 <pulkitmahajan.01@gmail.com>
2020-03-23 16:33:33 +0530

    new java class added and added firebase functionlities

--- app/src/main/java/com/gamecodeschool/covid/homePage1.java
+++ app/src/main/java/com/gamecodeschool/covid/homePage1.java
@@ -8,11 +8,16 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.text.DateFormat;
 import java.util.Calendar;
@@ -20,7 +25,11 @@ import java.util.Calendar;
 public class homePage1 extends AppCompatActivity implements DatePickerDialog.OnDateSetListener, AdapterView.OnItemSelectedListener {
     EditText etName,etAge,etAddress,etSubsidaryPhones;
     ImageView ivCld;
+    Button btnNext1;
+    Spinner spGender;
 
+  //  public FirebaseAuth mAuth;
+ //   public DatabaseReference databaseDrivers;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +38,13 @@ public class homePage1 extends AppCompatActivity implements DatePickerDialog.OnD
         etName=findViewById(R.id.etName);
         etAge=findViewById(R.id.etAge);
         ivCld=findViewById(R.id.ivCld);
+        btnNext1=findViewById(R.id.btnNext1);
+        etAddress=findViewById(R.id.etAddress);
+        etSubsidaryPhones=findViewById(R.id.etSubsidaryPhones);
+
+//        databaseDrivers= FirebaseDatabase.getInstance().getReference("User_Info");
+    //    mAuth=FirebaseAuth.getInstance();
+
         ivCld.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -36,12 +52,45 @@ public class homePage1 extends AppCompatActivity implements DatePickerDialog.OnD
                 datePicker.show(getSupportFragmentManager(),"date picker");
             }
         });
-        Spinner spinner=findViewById(R.id.spGender);
+        final Spinner spinner=findViewById(R.id.spGender);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.sex, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
         spinner.setOnItemSelectedListener(this);
+
+        btnNext1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String Name,Dob,Gen,Address,SubPhn;
+                Name=etName.getText().toString().trim();
+                Dob=etAge.getText().toString().trim();
+                Gen=spGender.getSelectedItem().toString().trim();
+                Address=etAddress.getText().toString().trim();
+                SubPhn=etSubsidaryPhones.getText().toString().trim();
+
+                if (Name.isEmpty()||Dob.isEmpty()||(SubPhn.length()<10)||Gen.isEmpty()||Address.isEmpty())
+                    Toast.makeText(getApplicationContext(),"All fields are mandatory",Toast.LENGTH_LONG).show();
+                else{
+                    btnNext1.setVisibility(View.VISIBLE);
+                   // addCredentials();
+                }
+            }
+
+        });
     }
+ /*   public void addCredentials()
+    {
+
+        String dvAdd,dvPhn,dvName,dvGen,dvDob = new String();
+        String dvid= databaseDrivers.push().getKey();
+        dvAdd=etAddress.getText().toString().trim();
+        dvPhn=etSubsidaryPhones.getText().toString().trim();
+        dvName=etName.getText().toString().trim();
+       dvGen=spGender.getSelectedItem().toString().trim();
+        dvDob=etAge.getText().toString().trim();
+        user_info dCredentials= new user_info(dvAdd,dvPhn,dvName,dvGen,dvDob);
+        databaseDrivers.child(dvid).setValue(dCredentials);
+    }*/
 
     @Override
     public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
