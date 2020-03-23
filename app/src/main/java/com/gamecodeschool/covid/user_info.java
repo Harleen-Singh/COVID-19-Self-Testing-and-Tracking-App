15e6b7b055c0526007a952cdce188fbb27bd8f81
Pulkitmahajan01 <pulkitmahajan.01@gmail.com>
2020-03-23 16:38:43 +0530

    firebase and new java class

---
+++ app/src/main/java/com/gamecodeschool/covid/user_info.java
@@ -0,0 +1,22 @@
+package com.gamecodeschool.covid;
+
+public class user_info {
+    String dName,dGen,dDob,dAdd,dPhn;
+
+    public user_info(String dName,String dGen, String dDob,String dAdd,String dPhn){
+        this.dName = dName;
+        this.dGen = dGen;
+        this.dDob=dDob;
+        this.dAdd=dAdd;
+        this.dPhn=dPhn;
+    }
+    public String getdName() {
+        return dName;
+    }
+    public String getdGen(){ return dGen; }
+    public String getdDob(){ return dDob;}
+        public String getdAdd(){return dAdd;}
+        public String getdPhn(){return dPhn;}
+}
+
+
